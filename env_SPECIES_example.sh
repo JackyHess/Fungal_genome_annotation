7c3dcdee5d5477bc4d5594e91e897d91696cafc1
JackyHess <jaqueline.hess@gmail.com>
2016-04-14 15:42:51 +0200

    Create env_SPECIES_example.sh

---
+++ env_SPECIES_example.sh
@@ -0,0 +1,7 @@
+export JAMG_PATH=/usit/abel/u1/jacqueh/Software/jamg
+export GENOME_PATH=/projects/researchers/researchers01/jacqueh/Genome_annotation/SHA17-1/Sshas_SH17-1.fa
+export GENOME_NAME=Sshas_SHA17-1
+export LOCAL_CPUS=10
+export MAX_MEMORY_G=60
+export PATH=$JAMG_PATH/bin:$JAMG_PATH/3rd_party/bin:$PATH
+export MAX_INTRON_LENGTH=300
