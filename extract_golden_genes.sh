f0259674af7e2a115895ffa29a06e4a430ca48a5
JackyHess <jaqueline.hess@gmail.com>
2016-04-19 16:05:56 +0200

    Update extract_golden_genes.sh

--- extract_golden_genes.sh
+++ extract_golden_genes.sh
@@ -5,4 +5,6 @@
 #SBATCH --mem-per-cpu=500
 #SBATCH -t 500
 
+module load emboss
+
 $JAMG_PATH/bin/prepare_golden_genes_for_predictors.pl -genome $GENOME_PATH.masked -softmasked $GENOME_PATH.softmasked -same_species -intron $MAX_INTRON_LENGTH -cpu $LOCAL_CPUS -norefine -complete -no_single -pasa_gff ./*.assemblies.fasta.transdecoder.gff3 -pasa_peptides ./*.assemblies.fasta.transdecoder.pep -pasa_cds ./*.assemblies.fasta.transdecoder.cds -pasa_genome ./*.assemblies.fasta.transdecoder.genome.gff3 -pasa_assembly ./*.assemblies.fasta
