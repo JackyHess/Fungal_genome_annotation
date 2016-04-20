f58b6de2f2ae2adffbf0f8e8007c07700487d5d6
JackyHess <jaqueline.hess@gmail.com>
2016-04-20 09:54:15 +0200

    Create extract_best_hits.sh

---
+++ extract_best_hits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=10
+#SBATCH --account=uio
+#SBATCH --mem-per-cpu=500
+#SBATCH -t 500
+
+makeblastdb -in all_reference_proteins.fasta.nr90 -dbtype prot -out all_reference_proteins.fasta.nr90
+
+python /usit/abel/u1/jacqueh/Code/GenomeAnnotation/filter_agaricomycete_protein_set.py $GENOME_PATH all_reference_proteins.fasta.nr90 all_reference_protein
+s.fasta.nr90 best_matches_agaricomycetes.fa tmp_blast/
