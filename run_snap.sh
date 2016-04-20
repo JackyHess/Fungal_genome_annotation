3e11580bd48569c39954d2acdaac00d8e117e742
JackyHess <jaqueline.hess@gmail.com>
2016-04-20 10:11:51 +0200

    Update run_snap.sh

--- run_snap.sh
+++ run_snap.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 #SBATCH --cpus-per-task=10
@@ -5,15 +6,16 @@
 #SBATCH --mem-per-cpu=3000
 #SBATCH -t 200
 
+module load cufflinks #for gffread utility
 
 $JAMG_PATH/bin/splitfasta.pl -i $GENOME_NAME.softmasked
 
-find $GENOME_NAME.softmasked_dir1 -maxdepth 1 -type f -exec sh -c 'echo "$JAMG_PATH/3rd_party/snap/snap ../train/$GENOME_NAME.hmm $1 -lcmask -quiet > $1.snap 2>/dev/null;$JAMG_PATH/3rd_party/evidencemodeler/OtherGeneFinderTrainingGuide/SNAP/SNAP_output_to_gff3.pl $1.snap $1 > $1.snap.gff3 2>/dev/null;$JAMG_PATH/3rd_party/PASA/misc_utilities/gff3_to_gtf_format.pl $1.snap.gff3 $1 > $1.snap.gtf 2>/dev/null "' find-copy '{}' \; > snap.commands
+find $GENOME_NAME.softmasked_dir1 -maxdepth 1 -type f -exec sh -c 'echo "$JAMG_PATH/3rd_party/snap/snap ../$GENOME_NAME.hmm $1 -lcmask -quiet > $1.snap 2>$
 
 ParaFly -c snap.commands -CPU $LOCAL_CPUS -v –shuffle
 
-cat $GENOME_NAME.softmasked_dir1/*snap.gtf > snap.gtf 
+cat $GENOME_NAME.softmasked_dir1/*snap.gtf > snap.gtf
 
-gffread final_golden_genes.gff3.nr.golden.test.gff3 -T -o final_golden_genes.gff3.nr.golden.test.gtf
+gffread ../../../PASA/final_golden_genes.gff3.nr.golden.test.gff3 -T -o final_golden_genes.gff3.nr.golden.test.gtf
 
-$JAMG_PATH/3rd_party/eval-2.2.8/evaluate_gtf.pl -g ../../PASA/*golden.test.gtf snap.gtf > snap.eval
+$JAMG_PATH/3rd_party/eval-2.2.8/evaluate_gtf.pl -g final_golden_genes.gff3.nr.golden.test.gtf snap.gtf > snap.eval
