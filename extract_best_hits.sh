4f86c2357806c4b056ec5408076dd441fb70fb8e
JackyHess <jaqueline.hess@gmail.com>
2016-04-21 15:06:46 +0200

    Update extract_best_hits.sh

--- extract_best_hits.sh
+++ extract_best_hits.sh
@@ -7,5 +7,4 @@
 
 makeblastdb -in all_reference_proteins.fasta.nr90 -dbtype prot -out all_reference_proteins.fasta.nr90
 
-python /usit/abel/u1/jacqueh/Code/GenomeAnnotation/filter_agaricomycete_protein_set.py $GENOME_PATH all_reference_proteins.fasta.nr90 all_reference_protein
-s.fasta.nr90 best_matches_agaricomycetes.fa tmp_blast/
+python /usit/abel/u1/jacqueh/Code/GenomeAnnotation/filter_agaricomycete_protein_set.py $GENOME_PATH all_reference_proteins.fasta.nr90 all_reference_proteins.fasta.nr90 best_matches_agaricomycetes.fa tmp_blast/
