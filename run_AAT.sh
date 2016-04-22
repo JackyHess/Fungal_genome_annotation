d5fd2f660f4af305aa7a8db116e8997ba5595d84
JackyHess <jaqueline.hess@gmail.com>
2016-04-22 10:07:50 +0200

    Create run_AAT.sh

---
+++ run_AAT.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH -t 3000
+#SBATCH --mem-per-cpu=16000
+#SBATCH --account=uio
+
+$JAMG_PATH/3rd_party/aatpackage/bin/AAT.pl -P -q $GENOME_PATH.masked --unmasked $GENOME_PATH -s best_matches_agaricomycetes.fa --dps ‘-f 100 -i 30 -a 200’ --filter ‘-c 10’ --nap ‘-x 5’
+
+cat *best_hits_agaricomycetes.fa.nap.gff3 > $GENOME_NAME_best_hits_agaricomycetes.gff3
