6ee832a0244e5d67952a02c07ea2e76fe3f29302
JackyHess <jaqueline.hess@gmail.com>
2016-04-25 13:56:46 +0200

    Update run_AAT.sh

--- run_AAT.sh
+++ run_AAT.sh
@@ -7,4 +7,4 @@
 
 $JAMG_PATH/3rd_party/aatpackage/bin/AAT.pl -P -q $GENOME_PATH.masked --unmasked $GENOME_PATH -s best_matches_agaricomycetes.fa --dps ‘-f 100 -i 30 -a 200’ --filter ‘-c 10’ --nap ‘-x 5’
 
-cat *best_hits_agaricomycetes.fa.nap.gff3 > $GENOME_NAME_best_hits_agaricomycetes.gff3
+cat *best_matches_agaricomycetes.fa.nap.gff3 > $GENOME_NAME.best_hits_agaricomycetes.gff3
